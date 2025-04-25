8a880240beb1c22a728bb330a49adb19c6b718b1
quantran139 <tranhongquan13999@gmail.com>
2025-04-25 15:13:10 +0700

    update cicd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,6 +8,10 @@ git checkout $BRANCH
 git pull
 
 docker compose -f docker-compose.yml down
+# Pull 2 image mới nhất từ Docker Hub
 docker compose -f docker-compose.yml pull discoveryserver
-docker compose -f docker-compose.yml up -d discoveryserver
+docker compose -f docker-compose.yml pull bookservice
+
+# Khởi động lại 2 container mới pull
+docker compose -f docker-compose.yml up -d discoveryserver bookservice
 docker system prune -af
